a095fa60d4545d14397e44628e714a7e3bc73450
MZD20 <mnava35@ucsc.edu>
2026-02-05 14:04:28 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+//Wall Drawing 365
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(180);
+  
+  square(0, 0, 200);
+ 
+  square(200, 0, 0);
+  fill(150);
+  
+  
+  square(0, 200, 200);
+  fill(100);
+ 
+  square(200, 200, 200);
+  fill(200);
+ 
+  
+}
